7043c46c3f1120f64c9fc753541f93fc6be6bdcc
fdsayre <frank.darrow@gmail.com>
2018-08-20 09:33:47 -0500

    changes to presentation

---
+++ SCSession-Starting-With-Data_CODE.R
@@ -0,0 +1,21 @@
+# Code from: http://datacarpentry.org/R-ecology-lesson/02-starting-with-data.html 
+
+download.file("https://ndownloader.figshare.com/files/2292169", "data/portal_data_joined.csv")
+
+surveys <- read.csv("portal_data_joined.csv")
+
+head(surveys)
+
+View(surveys)
+
+str(surveys)
+
+
+dim(surveys)
+nrow(surveys)
+ncol(surveys)
+ 
+rownames(surveys)
+
+str(surveys)
+summary(surveys)
