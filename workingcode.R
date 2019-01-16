ea6d156c5c7cec6ebd4cbb7c74fda56b41a48be8
fdsayre <frank.darrow@gmail.com>
2019-01-16 13:57:55 -0600

    deleting lots of files, starting viz section

--- workingcode.R
+++ workingcode.R
@@ -22,7 +22,7 @@ download.file("https://raw.githubusercontent.com/fdsayre/TeachingRIntro50Minutes
 
 # Setting up ---------------
 # set up a project 
-#
+
 # Use FILE --> New Project
 
 # set up your working directory so R knows where to look for files. You can do this with the following command or by using the menu Session --> Set Working Directory
@@ -57,11 +57,11 @@ z
 # loading from a CSV file
 # this example data is from 
 
-# get the csv file
+# download the csv file into a *data* directory
 
 download.file("https://raw.githubusercontent.com/fdsayre/TeachingRIntro50Minutes/master/gapminder.csv", "data/gapminder.csv")
 
-# load it into an object
+# load the data into an object called
 
 csv_data <- read.csv("data/gaptestdownload.csv")
 
@@ -69,7 +69,7 @@ csv_data <- read.csv("data/gaptestdownload.csv", header=TRUE)
 
 csv_data <- read.csv("data/gaptestdownload.csv", header=TRUE, na.strings=c("N/A", "Not Available", "NA", "0"))
 
-# using built-in datasets 
+# ALTERNATIVE: use the built-in datasets 
 
 install.packages("gapminder")
 library(gapminder)
@@ -99,30 +99,36 @@ seq(1, 10)
 seq(1, 10, by = 2)
 
 gap[seq(1, NROW(gap), by = 10),]
+
+
 # Plotting Data ------------
 
 library(ggplot2)
 
 
 
-plot(data$energy_star_score)
 
-hist(data$energy_star_score)
+hist(csv_data$lifeExp)
 
-boxplot(data$energy_star_score)
+boxplot(csv_data$lifeExp ~ csv_data$continent)
 
-boxplot(data$energy_star_score ~ data$org_name)
-
-
-boxplot(data$energy_star_score ~ data$public_private)
+plot(csv_data$lifeExp ~ csv_data$gdpPercap)
 
 
-ggplot(cardata, aes(x=hp, y=mpg, color=gear)) +
-  geom_point()
+# move to ggplot2
+# ggplot2 is 
 
-ggplot(gap, aes(y=lifeExp, x=gdpPercap, color=continent)) +
+ggplot(csv_data, aes(y=lifeExp, x=gdpPercap)) +
+  geom_point() 
+
+ggplot(csv_data, aes(y=lifeExp, x=gdpPercap)) +
+  geom_point() +
+  scale_x_log10()
+
+ggplot(csv_data, aes(y=lifeExp, x=gdpPercap, color=continent)) +
   geom_point() +
   scale_x_log10()
 
 # Getting Data Out of R ------------
 
+write.csv(csv_data, file = "my_exported_data.csv")
