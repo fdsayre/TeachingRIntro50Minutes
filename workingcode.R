46c130f0c1c5516ebc0b22df9c8a7aa90d27ebaf
fdsayre <frank.darrow@gmail.com>
2019-01-17 16:35:10 -0600

    fixing post jan2019 class issues

--- workingcode.R
+++ workingcode.R
@@ -16,10 +16,6 @@
 # - running in console vs. in a file
 ###################
 
-# Get a copy of this code to follow along
-
-download.file("https://raw.githubusercontent.com/fdsayre/TeachingRIntro50Minutes/master/workingcode.R", "introcode.R")
-
 # console vs. text file
 
 5 + 5
@@ -38,7 +34,7 @@ setwd("~/projects/TeachingRIntro50Minutes")
 
 # we also need to install any packages we are going to use. You typically only need to do this once per package and in future sessions you won't need to do it. 
 
-install.packages("ggplot")
+install.packages("ggplot2")
 
 # then we need to load libraries. we need to do this once for every session. 
 
@@ -66,28 +62,26 @@ z
 
 # download the csv file into a *data* directory
 
-download.file("https://raw.githubusercontent.com/fdsayre/TeachingRIntro50Minutes/master/data/gapminder.csv", "data/gapminder.csv")
+download.file("https://raw.githubusercontent.com/fdsayre/TeachingRIntro50Minutes/master/data/gapminder.csv", "gapminder.csv")
 
 # load the data into an object called
 
-csv_data <- read.csv("data/gaptestdownload.csv")
+# just load in the CSV
+csv_data <- read.csv("gapminder.csv")
 
-csv_data <- read.csv("data/gaptestdownload.csv", header=TRUE)
+# explicitely tell R the first row is column names
+csv_data <- read.csv("gapminder.csv", header=TRUE)
 
-csv_data <- read.csv("data/gaptestdownload.csv", header=TRUE, na.strings=c("N/A", "Not Available", "NA", "0"))
-
-# ALTERNATIVE: use the built-in datasets 
-
-install.packages("gapminder")
-library(gapminder)
-
-gap <- gapminder
-gap
+# tell R what strings are actually NA values
+csv_data <- read.csv("gapminder.csv", header=TRUE, na.strings=c("N/A", "Not Available", "NA", "0"))
 
 
 # Exploring Your Data ------------
 
 str() #see structure
+
+str(csv_data)
+
 head() #see head
 tail() #see tail
 dim() #see dimensions 
@@ -97,24 +91,19 @@ summary() #summary stats
 
 # subsetting data
 
-gap[1,]
-gap[,1:2]
+csv_data[1,]
+csv_data[,1:2]
 
-gap[c(1, 2, 50, 100),]
+csv_data[c(1, 2, 50, 100),]
 
-seq(1, 10)
-seq(1, 10, by = 2)
-
-gap[seq(1, NROW(gap), by = 10),]
-
-
-# Plotting Data with ggplot ------------
+# Plotting Data with built in plotting functions and ggplot ------------
 # https://www.rstudio.com/wp-content/uploads/2015/03/ggplot2-cheatsheet.pdf
 
-library(ggplot2)
+(ggplot2)
 
 
 
+# built in plotting
 
 hist(csv_data$lifeExp)
 
@@ -122,27 +111,23 @@ boxplot(csv_data$lifeExp ~ csv_data$continent)
 
 plot(csv_data$lifeExp ~ csv_data$gdpPercap)
 
-
-# move to ggplot2
-# ggplot2 is 
+# ggplot
+library(ggplot2)
 
 ggplot(csv_data, aes(y=lifeExp, x=gdpPercap)) +
   geom_point() 
 
+# put on a logarithmic scale 
 ggplot(csv_data, aes(y=lifeExp, x=gdpPercap)) +
   geom_point() +
   scale_x_log10()
 
+#Add color for continents 
 ggplot(csv_data, aes(y=lifeExp, x=gdpPercap, color=continent)) +
   geom_point() +
   scale_x_log10()
 
-# what else might we want to look at? What other variables are there?
-str(csv_data)
-eom_bar()
-geom_histogram()
-geom_smooth()
-
+# try adding size for population!
 
 # DPLYR -----------
 # https://www.rstudio.com/wp-content/uploads/2015/02/data-wrangling-cheatsheet.pdf
@@ -151,8 +136,11 @@ library(dplyr)
 
 # filter = choose rows based on criteria 
 
-filter(csv_data, lifeExp < 29)   
-filter(gapminder, country == "Canada")
+Africa_only <- filter(csv_data, continent == "Africa")
+
+ggplot(Africa_only, aes(y=lifeExp, x=gdpPercap, size=pop)) +
+  geom_point() +
+  scale_x_log10()
 
 # select = choose columns 
 
